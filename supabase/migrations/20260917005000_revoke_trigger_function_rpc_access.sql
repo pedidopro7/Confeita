bab667600d63ca5aef233cbaa8ac9ddb0e2995cf
pedidopro7 <pedidopro7@gmail.com>
2026-09-16 21:43:09 -0300

    db: revoke public RPC access from trigger functions

---
+++ supabase/migrations/20260917005000_revoke_trigger_function_rpc_access.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.create_owner_membership() from public, anon, authenticated;
+revoke execute on function public.rls_auto_enable() from public, anon, authenticated;
+revoke execute on function public.set_updated_at() from public, anon, authenticated;
